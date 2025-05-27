c4ba1dfd51a7a2fc7a26b9e9649e9b767ea2eb0b
DevFajardo <devfajardocadena@gmail.com>
2025-05-27 03:41:55 -0500

    login

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,15 @@
-import Logo from '@/components/Logo';
-import { useRouter } from 'expo-router';
-import { useEffect, useState } from 'react';
-import { ImageBackground, StyleSheet, Text, View } from 'react-native';
-import Button from '../components/Button';
-import Input from '../components/Input';
+import Logo from "@/components/Logo";
+import { useRouter } from "expo-router";
+import { useEffect, useState } from "react";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import Button from "../components/Button";
+import Input from "../components/Input";
 
 export default function LoginScreen() {
   const router = useRouter();
   const [mostrarFormulario, setMostrarFormulario] = useState(false);
+  const [usuario, setUsuario] = useState("");
+  const [contrasena, setContrasena] = useState("");
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -18,27 +20,54 @@ export default function LoginScreen() {
   }, []);
 
   const handleLogin = () => {
-    // Aquí puedes validar el login más adelante
-    router.push("/(tabs)");
+    if (usuario === "" || contrasena === "") {
+      alert("Por favor, complete todos los campos");
+      setContrasena("");
+      setUsuario("");
+      return;
+    }
+    if (usuario === "admin" && contrasena === "admin") {
+      setUsuario("");
+      setContrasena("");
+      router.push("/(tabs)");
+      return;
+    } else {
+      alert("Credenciales inválidas");
+      setContrasena("");
+      setUsuario("");
+      return;
+    }
   };
 
   if (!mostrarFormulario) {
     return (
       <ImageBackground
-        source={require('@/assets/images/Sp.png')} // Cambia la ruta si es necesario
+        source={require("@/assets/images/Sp.png")} // Cambia la ruta si es necesario
         style={styles.imagenFondo}
       />
     );
   }
   return (
     <View style={styles.container}>
-      <View style ={styles.containerLogo}>
-      <Logo/>
+      <View style={styles.containerLogo}>
+        <Logo />
       </View>
       <Text style={styles.text}>Usuario:</Text>
-      <Input placeholder="Usuario" keyboardType="email-address" autoCapitalize="none" />
+      <Input
+        placeholder="Usuario"
+        keyboardType="email-address"
+        autoCapitalize="none"
+        value={usuario}
+        onChangeText={setUsuario}
+      />
       <Text style={styles.text}>Contraseña:</Text>
-      <Input placeholder="Contraseña" secureTextEntry autoCapitalize="none" />
+      <Input
+        placeholder="Contraseña"
+        secureTextEntry
+        autoCapitalize="none"
+        value={contrasena}
+        onChangeText={setContrasena}
+      />
       <Button title="Ingresar" onPress={handleLogin} />
     </View>
   );
@@ -47,25 +76,24 @@ export default function LoginScreen() {
 const styles = StyleSheet.create({
   imagenFondo: {
     flex: 1,
-    resizeMode: 'cover',
-    justifyContent: 'center',
-    alignItems: 'center',
+    resizeMode: "cover",
+    justifyContent: "center",
+    alignItems: "center",
   },
-  containerLogo:{
-    paddingTop:10
+  containerLogo: {
+    paddingTop: 10,
   },
   container: {
-    flex:1,
-    backgroundColor: '#fff',
-    justifyContent: 'flex-start',
-    padding:20,
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "flex-start",
+    padding: 20,
     paddingTop: 100,
   },
   text: {
-    margin:10,
-    paddingLeft:5,
-    fontSize:15,
-    fontWeight: 'bold'
+    margin: 10,
+    paddingLeft: 5,
+    fontSize: 15,
+    fontWeight: "bold",
   },
- 
 });
